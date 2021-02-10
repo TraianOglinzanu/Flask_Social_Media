57defc581acd6c29603ee1823c039e476833c00e
Traian Oglinzanu <alex.traian.oglinzanu@gmail.com>
2021-02-09 20:24:39 -0700

    database setup

--- hockeyblog/__init__.py
+++ hockeyblog/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URAI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = get_flashed_messages()
+
+db = SQLAlchemy(app)
+Migrate(app)
+
 from hockeyblog.core.views import core
+from hockeyblog.error_pages.handlers import error_pages
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
