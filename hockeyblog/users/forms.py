24721ffe0be382955ad8c8a27011e0870a0f532c
Traian Oglinzanu <alex.traian.oglinzanu@gmail.com>
2021-02-11 19:40:31 -0700

    setting up forms

---
+++ hockeyblog/users/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from hockeyblog.models import User 
+
+class LoginForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired(),Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	submit = SubmitField('Log in')
