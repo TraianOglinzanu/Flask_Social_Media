767919775e52790ec45a4668ba1b50346ddb0b6e
Traian Oglinzanu <alex.traian.oglinzanu@gmail.com>
2021-02-11 19:47:12 -0700

    still forms

--- hockeyblog/users/forms.py
+++ hockeyblog/users/forms.py
@@ -10,3 +10,58 @@ class LoginForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired(),Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	submit = SubmitField('Log in')
+
+class RegistrationForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired(),Email()])
+	username = StringField('UserName', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm')])
+	pass_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+
+	def check_email(self, field):
+		if User.query.filter_by(email=field.data).first():
+			raise ValidationError('This email has already been registered')
+
+	def check_username(self, field):
+		if User.query.filter_by(username=field.data).first():
+			raise ValidationError('This isername has already been registered')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
