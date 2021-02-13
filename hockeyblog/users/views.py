8fc6954d400294a101b240ba17b85524b04e11d5
Traian Oglinzanu <alex.traian.oglinzanu@gmail.com>
2021-02-12 19:04:55 -0700

    more views

--- hockeyblog/users/views.py
+++ hockeyblog/users/views.py
@@ -7,8 +7,26 @@ from hockeyblog.users.picture_handler import add_profile_pic
 
 users = Blueprint('users',__name__)
 
+@users.route('/register', methods=['GET','POST'])
+def register():
+	form = RegistrationForm()
 
+	if form.validate_on_submit():
+		user = User(email=form.email.data,
+			username=form.username.data,
+			password=form.pass_confirm)
 
+		db.session.add(user)
+		db.session.commit()
+		flash('Thank you for registering')
+		return redirect(url_for('users.login'))
+
+	return render_template('register.html', form=form)
+
+@users.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('core.index'))
 
 
 
