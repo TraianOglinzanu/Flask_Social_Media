c821b5694149625fdc6fc52447853633f0a0d358
Traian Oglinzanu <alex.traian.oglinzanu@gmail.com>
2021-02-14 20:23:54 -0700

    more views

--- hockeyblog/users/views.py
+++ hockeyblog/users/views.py
@@ -9,6 +9,7 @@ users = Blueprint('users',__name__)
 
 @users.route('/register', methods=['GET','POST'])
 def register():
+
 	form = RegistrationForm()
 
 	if form.validate_on_submit():
@@ -25,10 +26,34 @@ def register():
 
 @users.route('/logout')
 def logout():
+
 	logout_user()
 	return redirect(url_for('core.index'))
 
+@users.route('/account', methods=['GET','POST'])
+@login_required
+def account():
 
+	form = UpdateUserForm()
+	if form.validate_on_submit():
+
+		if form.picture.data:
+			username = current.user.username
+			pic = add_profile_pic(form.picture.data, username)
+			current_user.profile_image = pic
+
+		current_user.username = form.username.data 
+		current_user.email = form.email.data 
+		db.session.commit()
+		flash('Account Updated')
+		return redirect(url_for('users.account'))
+
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.email.data = current_user.email
+
+	profile_image = url_for('static', filename='profile_pics/'+ current_user.profile_image)
+	return render_template('account.html', profile_image=profile_image, form=form)
 
 
 
