736335485390c1235b29fc583a2cccd93b116958
Traian Oglinzanu <alex.traian.oglinzanu@gmail.com>
2021-02-20 20:53:36 -0700

    fixed issues

--- hockeyblog/models.py
+++ hockeyblog/models.py
@@ -12,9 +12,9 @@ class User(db.Model, UserMixin):
 	__tablename__ = 'users'
 
 	id = db.Column(db.Integer, primary_key=True)
-	profile_image = db.Column(db.String(100),nullable=False,default='default_prfile.png')
+	profile_image = db.Column(db.String(100),nullable=False,default='default_profile.png')
 	email = db.Column(db.String(100), unique=True,index=True)
-	username = db.column(db.String(100), unique=True,index=True)
+	username = db.Column(db.String(100), unique=True,index=True)
 	password_hash = db.Column(db.String(128))
 
 	posts = db.relationship('BlogPost', backref='author',lazy=True)
@@ -28,7 +28,7 @@ class User(db.Model, UserMixin):
 		return check_password_hash(self.password_hash,password)
 
 	def __repr__(self):
-		return f"Username {self.username}"
+		return f"Username:  {self.username}"
 
 class BlogPost(db.Model):
 	
--- hockeyblog/users/forms.py
+++ hockeyblog/users/forms.py
@@ -7,11 +7,13 @@ from flask_login import current_user
 from hockeyblog.models import User 
 
 class LoginForm(FlaskForm):
+
 	email = StringField('Email', validators=[DataRequired(),Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	submit = SubmitField('Log in')
 
 class RegistrationForm(FlaskForm):
+
 	email = StringField('Email', validators=[DataRequired(),Email()])
 	username = StringField('UserName', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm')])
@@ -19,18 +21,31 @@ class RegistrationForm(FlaskForm):
 	submit = SubmitField('Register')
 
 	def check_email(self, field):
+
 		if User.query.filter_by(email=field.data).first():
 			raise ValidationError('This email has already been registered')
 
 	def check_username(self, field):
+
 		if User.query.filter_by(username=field.data).first():
-			raise ValidationError('This isername has already been registered')
+			raise ValidationError('This username has already been registered')
 
 class UpdateUserForm(FlaskForm):
+
 	email = StringField('Email', validators=[DataRequired(),Email()])
 	username = StringField('Email', validators=[DataRequired()])
 	picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
 	submit = SubmitField('Update')
+
+	def check_email(self, field):
+
+		if User.query.filter_by(email=field.data).first():
+			raise ValidationError('This email has already been registered')
+
+	def check_username(self, field):
+
+		if User.query.filter_by(username=field.data).first():
+			raise ValidationError('This username has already been registered')
 
 
 
